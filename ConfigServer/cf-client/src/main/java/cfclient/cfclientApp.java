37f5ddb1fbd796486ddba7a1c66b509c7c6a5b23
mwm_jusfoun <mwm@jusfoun.com>
2018-09-12 18:25:20 +0800

    添加rmq 动态读取配置信息消息总线(Spring Cloud Bus)

--- ConfigServer/cf-client/src/main/java/cfclient/cfclientApp.java
+++ ConfigServer/cf-client/src/main/java/cfclient/cfclientApp.java
@@ -3,6 +3,9 @@ package cfclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Hello world!
  *
- */
+ */ 
 
 @SpringBootApplication
 @RestController
+@RefreshScope
 public class cfclientApp 
 {
 	public static void main(String[] args) {
